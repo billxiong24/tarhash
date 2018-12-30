aae25bb6fe9a78eb6b74f82c73b3f994ca8c190d
billxiong24 <billx0477@gmail.com>
2018-12-29 20:00:43 -0500

    packaging, doesn't work entirely

---
+++ tarhash.py
@@ -0,0 +1,88 @@
+import tarfile
+import hashlib
+import subprocess
+import sys
+import os
+import logging
+
+logging.getLogger().setLevel(logging.INFO)
+
+def _generate_hashes(fd):
+    CHUNK_SIZE = 1024 * 100
+    calc_hash = hashlib.md5()
+    data = fd.read(CHUNK_SIZE)
+    while data:
+        calc_hash.update(data)
+        data = fd.read(CHUNK_SIZE)
+
+    fd.close()
+    return calc_hash.hexdigest()
+
+def write_tar_sums_file(in_dir, tar_file, out_file=None, to_write=False):
+    if not os.path.isdir(in_dir):
+        logging.error("Target directory %s does not exist." % (in_dir))
+        return 1
+
+    if out_file is None and to_write is True:
+        logging.error("Must specify a valid output file for hashes.")
+        return 1
+
+    if out_file is not None:
+        to_write = True
+
+    cmd=""
+    if to_write is False:
+        cmd = "tar -cvpf %s %s | xargs -I '{}' sh -c \"test -f '{}' && md5sum '{}'\" | cut -f 1 -d ' '" % (tar_file, in_dir)
+    else:
+        cmd = "tar -cvpf %s %s | xargs -I '{}' sh -c \"test -f '{}' && md5sum '{}'\" | cut -f 1 -d ' ' > %s" % (tar_file, in_dir, out_file)
+
+    exit_code = subprocess.call(cmd, shell=True)
+    if exit_code is 0:
+        logging.info("Tar file and hashes successfully created.")
+        return 0
+
+    logging.error("Something went wrong when creating tar file.")
+    return 1
+
+def calc_tar_sums_from_tar(input_file, out_file=sys.stdout):
+    out_fd = None
+    if out_file is sys.stdout:
+        out_fd = sys.stdout
+    else:
+        out_fd = open(out_file, 'w')
+
+    if out_file is not sys.stdout:
+        logging.info("Opened '%s' for writing..." % (out_file))
+
+    hashes = []
+    try:
+        if not tarfile.is_tarfile(input_file):
+            logging.error("File '%s' is not a tar file.\n" % (input_file))
+            return 1
+    except IOError, e:
+        logging.error("File '%s' does not exist.\n" % (input_file))
+        return 2
+
+    # file exists and is a tar file, open file in read and stream mode
+    tar_obj = tarfile.open(input_file, "r|*")
+    logging.info("Opened '%s' for reading..." % (input_file))
+    # can't do getmembers, because that reads through the entire stream
+    for member in tar_obj:
+        if not member.isfile():
+            continue
+        # file exists, don't have to check for existence
+        # can't do member.name, because that reads ahead of the stream
+        fd = tar_obj.extractfile(member)
+        # not a regular file nor some link
+        if fd is None:
+            continue
+        logging.info("Writing hash of %s..." % (member.name))
+        out_fd.write(_generate_hashes(fd) + "\n")
+
+    logging.info("Successfully calculated hashes for all files in '%s'" % (input_file))
+    tar_obj.close()
+    out_fd.close()
+    return 0
+
+# write_tar_sums_file("msgtb", "back2.tar", "hash_in.md5")
+# calc_tar_sums_from_tar("back2.tar", "hash_out.md5")
---
+++ tarhash_packager.py
@@ -0,0 +1,67 @@
+import tarhash
+import uuid
+import shutil
+import os
+import subprocess
+import tarfile
+import filecmp
+
+HASH_FNAME="tar_hashes"
+HASH_EXT=".HASH.TXT"
+TEMP_FNAME="temp"
+
+def package_dir(in_dir, tar_file):
+
+    rand_id = uuid.uuid4().hex
+    hash_file = "%s%s%s" %(HASH_FNAME, rand_id, HASH_EXT)
+    # contains the real data
+    tmp_tar = "%s%s"%(rand_id, tar_file)
+    res = tarhash.write_tar_sums_file(in_dir, tmp_tar, hash_file, to_write=True)
+
+    # something went wrong
+    if res is not 0:
+        return
+
+    # package tar and hash file in another tar
+    tar_package = tarfile.open(tar_file, "w:gz")
+    tar_package.add(tmp_tar)
+    tar_package.add(hash_file)
+    tar_package.close()
+    # clean up
+    os.remove(tmp_tar)
+    os.remove(hash_file)
+
+def unpackage_and_verify_tar(tar_file):
+    try:
+        if not tarfile.is_tarfile(tar_file):
+            logging.error("File '%s' is not a tar file.\n" % (tar_file))
+            return False
+    except IOError, e:
+        logging.error("File '%s' does not exist.\n" % (tar_file))
+        return False
+
+    tar_obj = tarfile.open(tar_file, "r|*")
+
+    hash_txt = ""
+    rand_id = uuid.uuid4().hex
+    calc_hash_fname = "%s%s%s" % (TEMP_FNAME, rand_id, HASH_EXT)
+    for member in tar_obj:
+        fd = tar_obj.extract(member)
+        # the tar file with all the data
+        if member.name.endswith(tar_file):
+            tarhash.calc_tar_sums_from_tar(member.name,calc_hash_fname) 
+            os.remove(member.name)
+        # the txt file with hashes
+        elif member.name.startswith(HASH_FNAME) and member.name.endswith(HASH_EXT):
+            hash_txt = member.name
+
+    tar_obj.close()
+
+    cmp_res = filecmp.cmp(hash_txt, calc_hash_fname)
+    #os.remove(calc_hash_fname)
+    #os.remove(hash_txt)
+
+    return cmp_res
+
+package_dir("env", "content.tar")
+print unpackage_and_verify_tar("content.tar")
